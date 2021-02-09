3dc2ef77b81190a4b5a2e464c7cb22e8cb0effed
Benedikt Geisler <benedikt.geisler@posteo.de>
2021-02-09 14:20:52 +0100

    Übung erstellt

--- Csharp_2021_4.4_Datenfelder/Form1.Designer.cs
+++ Csharp_2021_4.4_Datenfelder/Form1.Designer.cs
@@ -29,20 +29,84 @@ namespace Csharp_2021_4._4_Datenfelder
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.lstFeld = new System.Windows.Forms.ListBox();
+			this.btnAnzeigen1 = new System.Windows.Forms.Button();
+			this.btnAnzeigen2 = new System.Windows.Forms.Button();
+			this.lblAusgabe = new System.Windows.Forms.Label();
+			this.btnAnzeigen3 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
+			// 
+			// lstFeld
+			// 
+			this.lstFeld.FormattingEnabled = true;
+			this.lstFeld.Location = new System.Drawing.Point(12, 12);
+			this.lstFeld.Name = "lstFeld";
+			this.lstFeld.Size = new System.Drawing.Size(120, 199);
+			this.lstFeld.TabIndex = 0;
+			// 
+			// btnAnzeigen1
+			// 
+			this.btnAnzeigen1.Location = new System.Drawing.Point(309, 13);
+			this.btnAnzeigen1.Name = "btnAnzeigen1";
+			this.btnAnzeigen1.Size = new System.Drawing.Size(75, 23);
+			this.btnAnzeigen1.TabIndex = 1;
+			this.btnAnzeigen1.Text = "Anzeigen 1";
+			this.btnAnzeigen1.UseVisualStyleBackColor = true;
+			this.btnAnzeigen1.Click += new System.EventHandler(this.btnAnzeigen1_Click);
+			// 
+			// btnAnzeigen2
+			// 
+			this.btnAnzeigen2.Location = new System.Drawing.Point(309, 43);
+			this.btnAnzeigen2.Name = "btnAnzeigen2";
+			this.btnAnzeigen2.Size = new System.Drawing.Size(75, 23);
+			this.btnAnzeigen2.TabIndex = 2;
+			this.btnAnzeigen2.Text = "Anzeigen 2";
+			this.btnAnzeigen2.UseVisualStyleBackColor = true;
+			this.btnAnzeigen2.Click += new System.EventHandler(this.btnAnzeigen2_Click);
+			// 
+			// lblAusgabe
+			// 
+			this.lblAusgabe.AutoSize = true;
+			this.lblAusgabe.Location = new System.Drawing.Point(153, 182);
+			this.lblAusgabe.Name = "lblAusgabe";
+			this.lblAusgabe.Size = new System.Drawing.Size(35, 13);
+			this.lblAusgabe.TabIndex = 3;
+			this.lblAusgabe.Text = "label1";
+			// 
+			// btnAnzeigen3
+			// 
+			this.btnAnzeigen3.Location = new System.Drawing.Point(309, 73);
+			this.btnAnzeigen3.Name = "btnAnzeigen3";
+			this.btnAnzeigen3.Size = new System.Drawing.Size(75, 23);
+			this.btnAnzeigen3.TabIndex = 4;
+			this.btnAnzeigen3.Text = "Anzeigen 3";
+			this.btnAnzeigen3.UseVisualStyleBackColor = true;
+			this.btnAnzeigen3.Click += new System.EventHandler(this.btnAnzeigen3_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(397, 225);
+			this.Controls.Add(this.btnAnzeigen3);
+			this.Controls.Add(this.lblAusgabe);
+			this.Controls.Add(this.btnAnzeigen2);
+			this.Controls.Add(this.btnAnzeigen1);
+			this.Controls.Add(this.lstFeld);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ListBox lstFeld;
+		private System.Windows.Forms.Button btnAnzeigen1;
+		private System.Windows.Forms.Button btnAnzeigen2;
+		private System.Windows.Forms.Label lblAusgabe;
+		private System.Windows.Forms.Button btnAnzeigen3;
 	}
 }
 
--- Csharp_2021_4.4_Datenfelder/Form1.cs
+++ Csharp_2021_4.4_Datenfelder/Form1.cs
@@ -12,9 +12,131 @@ namespace Csharp_2021_4._4_Datenfelder
 {
 	public partial class Form1 : Form
 	{
+		private readonly Random r = new Random();
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void btnAnzeigen1_Click(object sender, EventArgs e)
+		{
+			// Datenfeld deklarieren
+			int[] a;
+
+			// Datenfeld erzeugen
+			a = new int[7];
+
+			// ListBox leeren
+			lstFeld.Items.Clear();
+
+			// For-Schleife über alle Einträge des Feldes
+			// Index beginnt wie bei der ListBox bei 0
+			for (int i = 0; i < 7; i++)
+			{
+				// Datenfeld mit Zufallszahlen befüllen
+				a[i] = r.Next(20, 31);
+				lstFeld.Items.Add(a[i]);
+			}
+		}
+
+		private void btnAnzeigen2_Click(object sender, EventArgs e)
+		{
+			// Datenfeld deklarieren und erzeugen
+			int[] a = new int[7];
+
+			// Variablen für Min- und Max-Werte deklarieren
+			int MaxWert, MinWert, MaxWertIndex, MinWertIndex;
+
+			// ListBox leeren
+			lstFeld.Items.Clear();
+
+			// Zufallszahlen erzeugen und in ListBox eintragen
+			for (int i = 0; i < a.Length; i++)
+			{
+				a[i] = r.Next(20, 31);
+				lstFeld.Items.Add(a[i]);
+			}
+
+			// Vorschlag 1: 
+			// - Datenfeld sortieren
+			//	- Index 0 = Niedrigster Wert
+			//	- Index 6 = Höchster Wert
+
+			// Vorschlag 2: 
+			// - Visual Studion besitzt eine Methode, die Min- und
+			//   Max-Werte zurückgibt
+
+			// MinWert = a.Min();
+			// MaxWert = a.Max();
+			// MinWertIndex = Array.IndexOf(a, MinWert);
+			// MaxWertIndex = Array.IndexOf(a, MaxWert);
+
+			// Vorschlag 3:
+			// - Zahl in Zwischenspeicher speichern und alle Elemente durchlaufen.
+			//   Falls Zahl größer (kleiner) als gespeicherte Zahl ist, gespeicherte 
+			//	 Zahl ersetzen
+
+			// Variablen initialisieren
+			MaxWert = a[0];
+			MinWert = a[0];
+			MaxWertIndex = 0;
+			MinWertIndex = 0;
+
+			// Feld durchsuchen
+			for (int i = 0; i < a.Length; i++)
+			{
+				// Prüfen, ob aktueller Wert größer als bisheriger 
+				// Max-Wert ist
+				if (a[i] > MaxWert)
+				{
+					MaxWert = a[i];
+					MaxWertIndex = i;
+				}
+
+				// Prüfen, ob aktueller Wert kleiner als bisheriger 
+				// Min-Wert ist
+				if (a[i] < MinWert)
+				{
+					MinWert = a[i];
+					MinWertIndex = i;
+				}
+			}
+
+			lblAusgabe.Text =
+				"Max. Wert: " + MaxWert + " bei Index " + MaxWertIndex + "\n" +
+				"Min. Wert: " + MinWert + " bei Index " + MinWertIndex;
+		}
+
+		private void btnAnzeigen3_Click(object sender, EventArgs e)
+		{
+			int[] a = new int[7], b;
+			int SuchIndex;
+
+			// Zufallszahlen erstellen und speichern
+			for (int i = 0; i < a.Length; i++)
+			{
+				a[i] = r.Next(20, 31);
+			}
+
+			// Kopie des bestehenden Feldes a erstellen und in b speichern
+			b = (int[])a.Clone();
+
+			// Feld sortieren
+			Array.Sort(b);
+
+			// Sortiertes Feld anzeigen
+			lstFeld.Items.Clear();
+			for (int i = 0; i < b.Length; i++)
+			{
+				lstFeld.Items.Add(b[i]);
+			}
+
+			// Index der gesuchten Zahl bestimmen
+			SuchIndex = Array.IndexOf(b, 25);
+
+			// Gesuchten Wert anzeigen
+			lblAusgabe.Text = "Gesuchter Wert 25 bei Index: " + SuchIndex;
 		}
 	}
 }
